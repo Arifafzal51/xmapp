19b96ec96cca0902ce5e448550ca8a311dc8bfbf
arif afzal <arif.afzal_cs20@gla.ac.in>
2021-11-25 16:20:29 +0530

    First JDBC lab

---
+++ src/Edemojdbc.java
@@ -0,0 +1,17 @@
+import java.sql.*;
+
+public class Edemojdbc {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        //oracle.jdbc.oracledriver
+        Connection con = DriverManager.getConnection( "jdbc:mysql://localhost:3306/GLAE21" ,  "root", "" );
+        Statement s= con.createStatement();
+        ResultSet rs = s.executeQuery( "select * from stu");
+        while(rs.next())
+        {
+            System.out.println(rs.getInt(  1)+rs.getString(2));
+        }
+
+        con.close();
+         }
+}
